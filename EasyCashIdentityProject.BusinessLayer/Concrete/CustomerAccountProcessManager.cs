0429f28affca7618596d994810f2d905e2a48027
Melih Akyıldız <melihakyildiz0206@gmail.com>
2023-05-10 22:34:52 +0300

    Business Katmanı ve tema dosyaları eklendi

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,45 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessService _customerAccountProcessService;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessService customerAccountProcessService)
+        {
+            _customerAccountProcessService = customerAccountProcessService;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+           _customerAccountProcessService.TDelete(t);
+        }
+
+        public List<CustomerAccountProcess> TGetAll()
+        {
+            return _customerAccountProcessService.TGetAll();    
+        }
+
+        public CustomerAccountProcess TGetById(int id)
+        {
+           return _customerAccountProcessService.TGetById(id);
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessService.TInsert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessService.TUpdate(t);
+        }
+    }
+}
